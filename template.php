78c29c2339a7f7628be762c2dcd0c9af97da6a3a
Riley Martine <riley.martine@protonmail.com>
2017-02-16 16:47:18 -0500

    restructure static file structure

--- template.php
+++ template.php
@@ -7,10 +7,10 @@
     <title>Cellular Automata</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="static/styles/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="css/style.css" rel="stylesheet">
+    <!-- Custom styles -->
+    <link href="static/styles/style.css" rel="stylesheet">
   </head>
 
   <body>
@@ -30,6 +30,6 @@
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="../js/bootstrap.min.js"></script>
+    <script src="static/scripts/bootstrap.min.js"></script>
   </body>
 </html>
