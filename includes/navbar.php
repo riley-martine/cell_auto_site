e4c331498a33e0d695c28a6a7ec4aadfd3f738ed
Riley Martine <riley.martine@protonmail.com>
2017-02-16 14:53:18 -0500

    Add website github link

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,15 +5,23 @@
   <a class="navbar-brand" href="#">Navigation</a>
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <ul class="navbar-nav mr-auto">
+      
       <li class="nav-item">
         <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
+
       <li class="nav-item">
-        <a class="nav-link" target="_blank" href="https://github.com/auhsoja/Cellular-Automata-GUI">Github</a>
+        <a class="nav-link" target="_blank" href="https://github.com/auhsoja/Cellular-Automata-GUI">Project Code</a>
       </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" target="_blank" href="https://github.com/riley-martine/cell_auto_site">Website Code</a>
+      </li>
+
       <li class="nav-item">
         <a class="nav-link" href="resources.php">Resources</a>
       </li>
+
     </ul>
   </div>
 </nav>
