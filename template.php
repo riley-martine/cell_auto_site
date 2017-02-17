b678dcb0976f4668025d4c8286e67676f3f75746
Riley Martine <maxm215@gmail.com>
2017-02-17 10:11:58 -0500

    Move script imports and <head> data into includes/ files

--- template.php
+++ template.php
@@ -1,35 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <title>Cellular Automata</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="static/styles/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles -->
-    <link href="static/styles/style.css" rel="stylesheet">
+    <?php include "includes/head.html";?>
   </head>
 
   <body>
 
-    <?php include 'includes/navbar.php' ?>
-    <div class="container">
+  <?php include 'includes/navbar.php' ?>
+  
+  <div class="container">
       <div class="jumbotron">
         <h1>Cellular Automata</h1>
         <p class="lead">Template here. Lorem ipsum etc</p>
       </div>
     </div>
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="static/scripts/bootstrap.min.js"></script>
+  <?php include_once 'includes/scripts.html';?>
   </body>
 </html>
