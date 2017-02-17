3f1cc61b82dbb5c1af4d3d91e08acd8149c78868
Riley Martine <riley.martine@protonmail.com>
2017-02-17 10:11:58 -0500

    Move script imports and <head> data into includes/ files

--- resources.php
+++ resources.php
@@ -1,13 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <title>Cellular Automata Resources</title>
-
-    <link href="static/styles/bootstrap.min.css" rel="stylesheet">
-    <link href="static/styles/style.css" rel="stylesheet">
+    <?php include "includes/head.html";?>
   </head>
 
   <body>
@@ -40,13 +35,6 @@
       </div>
     </div>
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="static/scripts/bootstrap.min.js"></script>
+  <?php include_once 'includes/scripts.html'; ?>
   </body>
 </html>
